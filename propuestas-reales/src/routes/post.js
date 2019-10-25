2ecbc3735b64d9c1872a66fecd89b1e52fe05695
AGUSTIN KREBS <akrebs2@uc.cl>
2019-10-25 00:25:12 -0300

    basic html for post's index and new

--- propuestas-reales/src/routes/post.js
+++ propuestas-reales/src/routes/post.js
@@ -2,9 +2,21 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
-router.get('posts', '/', async (ctx) => {
+router.get('posts.list', '/', async (ctx) => {
   const posts = await ctx.orm.post.findAll();
-  await ctx.render('posts/index', { posts });
+  await ctx.render('posts/index', {
+    posts,
+    newPostPath: ctx.router.url('posts.new')});
+});
+
+router.get('posts.new', '/new', async (ctx) => {
+  await ctx.render('posts/new', {
+    submitPostPath: ctx.router.url('items.create'),
+  });
+});
+
+router.post('posts.create', '/', async (ctx) => {
+  ctx.redirect(ctx.router.url('posts.list'));
 });
 
 module.exports = router;
