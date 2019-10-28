330460e61592caf449c0c77930602c27f3a09d81
AGUSTIN KREBS <akrebs2@uc.cl>
2019-10-27 23:06:13 -0300

    added color tags

--- src/assets/js/components/Index.jsx
+++ src/assets/js/components/Index.jsx
@@ -2,20 +2,25 @@ import React, { Component } from 'react';
 
 function ListItem(props) {
   const post = props.value;
-  const showMinistries = post.ministries.map((m) => <p class="area">{m}</p>);
+  const showMinistries = post.ministries.map((m) => <p className={"area-" + m.replace(/\ /g, "-").replace(/\,/g, "")}>{m}</p>);
   return (
     <div className="post-container">
-      <div className="areas-container">
-        <h4 className="area-header">Área(s):</h4>
-        {showMinistries}
+      <div className="type-part-container">
+        <h4>{post.type}</h4>
       </div>
-      <div className="body-container">
-        <h4 className="propuesta-header">Propuesta:</h4>
-        <p className="body">
-          "{post.body}"
-        </p>
+      <div className="lower-part-container">
+        <div className="areas-container">
+          <h4 className="area-header">Área(s):</h4>
+          {showMinistries}
+        </div>
+        <div className="body-container">
+          <h4 className="propuesta-header">Propuesta:</h4>
+          <p className="body">
+            "{post.body}"
+          </p>
+        </div>
+        <p class="id"># {post.id}</p>
       </div>
-      <p class="id">{post.id}</p>
     </div>
   ); 
 } 
