5a9ced7dc3520aa08716142cddf5581ebce191c0
AGUSTIN KREBS <akrebs2@uc.cl>
2019-10-28 18:06:41 -0300

    more css changes

--- src/assets/js/assets.js
+++ src/assets/js/assets.js
@@ -1,2 +1,4 @@
 import '../images/logo.png';
 import '../images/logo_horizontal_propuestas_reales.png';
+import '../images/logo_horizontal_propuestas_reales_black.png';
+import '../images/fondo_landing_page.jpg';
