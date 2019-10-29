21272bc8653d2297fd40cfaf3cf7bef9a20c341a
fbarrientosj <fnbarrientos@uc.cl>
2019-10-29 17:07:56 -0300

    API

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -4,11 +4,16 @@ const router = new KoaRouter();
 
 router.get('api.posts.list', '/', async (ctx) => {
   const postsList = await ctx.orm.post.findAll();
-  ctx.body = ctx.jsonSerializer(postsList)});
+  ctx.body = ctx.jsonSerializer('post', {
+    attributes: ['privacy', 'email', 'instagram', 'body', 'rating', 'type', 'status'],
+    }).serialize(postsList);
+});
 
 router.get('api.post.show', '/:id', async (ctx) => {
   const post = await ctx.orm.post.findByPk(ctx.params.id);
-  ctx.body = ctx.jsonSerializer(post)
+  ctx.body = ctx.jsonSerializer('post', {
+    attributes: ['privacy', 'email', 'instagram', 'body', 'rating', 'type', 'status'],
+    }).serialize(post);
 });
 
 module.exports = router;
