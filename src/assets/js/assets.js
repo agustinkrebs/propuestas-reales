0f9ebaf9ceca87ebafd2b67714967299841b9fe8
fbarrientosj <fnbarrientos@uc.cl>
2019-11-13 13:25:11 -0300

    unknown fix

--- src/assets/js/assets.js
+++ src/assets/js/assets.js
@@ -4,5 +4,4 @@ import '../images/logo_horizontal_propuestas_reales_black.png';
 import '../images/fondo_landing_page.jpg';
 import '../images/instagram_logo.png';
 import '../images/email_logo.png';
-import "./download.js";
 
