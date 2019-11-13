9bb752bd9c2a70d3164c50122175d352f792c050
VICENTE LIRA <vjlira>
2019-11-13 16:28:59 -0300

    affinity emojis, urgency slider ok

--- src/assets/js/containers/Vote.jsx
+++ src/assets/js/containers/Vote.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import VoteComponent from '../components/Vote';
+import {Slider} from 'react-md';
+import 'react-md/src/scss/_react-md.scss'
 
+const happy = "https://static.thenounproject.com/png/407445-200.png";
+const soso = "https://static.thenounproject.com/png/1538729-200.png";
+const sad = "https://static.thenounproject.com/png/1259652-200.png";
 function postObject(posts) {
   const rPosts = posts.reverse();
   const objectList = rPosts.map((p) => JSON.parse(p));
@@ -26,19 +31,25 @@ export default class Vote extends Component {
   constructor(props) {
     super(props);
     const posts = postObject(postList);
+    let styles = ['emoji', 'emoji', 'emoji'];
 
     this.state = {
       posts,
       nonVoted: posts,
       currentPost: posts[0],
-      urgency: 0,
-      affinity: 0,
+      urgency: -1,
+      affinity: -1,
       noMorePosts: false,
       userIp: null,
+      styles: styles,
     };
     this.handleChange = this.handleChange.bind(this);
     this.updateNonVoted = this.updateNonVoted.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.happyAffinity = this.happyAffinity.bind(this);
+    this.sadAffinity = this.sadAffinity.bind(this);
+    this.indifferentAffinity = this.indifferentAffinity.bind(this);
+    this.updateUrgency = this.updateUrgency.bind(this);
   }
 
   async componentDidMount() {
@@ -47,8 +58,33 @@ export default class Vote extends Component {
   }
 
   async handleChange(event) {
+    console.log(event.target.name);
+    console.log(event.target.value);
     await this.setState({ [event.target.name]: event.target.value });
   }
+
+  async sadAffinity() {
+    console.log("cambio afiniut");
+    this.setState({styles: ['emoji', 'emoji', 'emoji-elegida']});
+    this.setState({affinity: 1});
+  }
+
+  async indifferentAffinity() {
+    console.log("cambio afiniut");
+    this.setState({styles: ['emoji', 'emoji-elegida', 'emoji']});
+    this.setState({affinity: 2});
+  }
+
+  async happyAffinity() {
+    console.log("cambio afiniut");
+    this.setState({styles: ['emoji-elegida', 'emoji', 'emoji']});
+    this.setState({affinity: 3});
+  }
+
+  async updateUrgency(urgency) {
+    this.setState({ urgency });
+  }
+
 
   async updateNonVoted() {
     const { nonVoted } = this.state;
@@ -72,6 +108,7 @@ export default class Vote extends Component {
       body: data,
     });
     this.updateNonVoted();
+    await this.setState({ urgency: -1, affinity: -1, styles:['emoji', 'emoji', 'emoji']})
     event.preventDefault();
   }
 
@@ -86,13 +123,32 @@ export default class Vote extends Component {
         <div className="posts-container">
           <VoteComponent currentPost={this.state.currentPost} />
         </div>
+        <div className="votos">
+          <h1>¿Qué te parece?</h1>
+          <img className={this.state.styles[0]} src={happy} onClick={this.happyAffinity}/>
+          <img className={this.state.styles[1]} src={soso} onClick={this.indifferentAffinity}/>
+          <img className={this.state.styles[2]} src={sad} onClick={this.sadAffinity}/>
+          <h1>¿Qué tan urgente crees que es?</h1>
+          <div className="slider-bar">
+            <Slider
+              discrete
+              id="discreteDefault"
+              value={this.state.urgency}
+              onChange={this.updateUrgency}
+              max={5}
+              step={1}
+              discreteTicks={1}
+              valuePrecision={1}
+              />
+          </div>
+        </div>
+        
         <form onSubmit={this.onSubmit} encType="multipart/form-data">
           <button type="submit" className="votar-button">
                 Votar
           </button>
         </form>
-
-      </div>
-    );
+        </div>
+    )
   }
 }
